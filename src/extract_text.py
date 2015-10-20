862affc93d42ca7f1d59fc9eea9b286659a0f750
Mike Widner <mikewidner@gmail.com>
2015-10-20 14:49:48 -0700

    Successfully writing Alain Juppe results

--- src/extract_text.py
+++ src/extract_text.py
@@ -12,7 +12,6 @@ Mike Widner <mikewidner@stanford.edu>
 import os
 import csv
 import sys
-import json
 import argparse
 from bs4 import BeautifulSoup
 
@@ -21,7 +20,7 @@ def get_settings():
   parser.add_argument('-i', dest='input_dir', required=True, help='Input directory where site crawls exist')
   parser.add_argument('-s', '--sites', dest='site_info', required=True, help='A CSV file of crawled sites, metadata, and DOM structure for extraction')
   parser.add_argument('-o', dest='output_dir', required=True,
-                   help='Output directory')
+                   help='Output directory for results')
   return parser.parse_args()
 
 def get_site_info(csvfile):
@@ -31,27 +30,6 @@ def get_site_info(csvfile):
     for row in reader:
       site_info.append(row)
   return site_info
-
-def parse_selector(selector):
-  '''
-  The selectors in the CSV file should be JSON
-  example: '{"tag": "FOO", "attrs": {"class": "BAR"}}'
-  Format for BeautifulSoup's find_all() function
-  '''
-  return json.loads(selector)
-
-def extract_text(site_info, filename):
-  '''
-  Extract the actual text from the HTML
-  '''
-  soup = BeautifulSoup(open(filename), 'html.parser')
-  title = soup.title.contents
-  date = soup.find()
-  contents = soup.find_all(selector['tag'], attrs=selector['attrs'])
-  if len(contents) > 0:
-    for content in contents:
-      print(content.getText())
-  pass
 
 def find_files(root, subdirs, files):
   '''
@@ -68,6 +46,23 @@ def find_files(root, subdirs, files):
       files_to_parse.append(os.path.join(root,filename))
   return files_to_parse
 
+def extract_text(site_info, filename):
+  '''
+  Extract the actual text from the HTML
+  '''
+  results = dict()
+  soup = BeautifulSoup(open(filename), 'html.parser')
+  results['filename'] = filename
+  results['title'] = soup.title.contents[0]
+  # Fields in CSV with BeautifulSoup select() options
+  for item in ['date','author','content']:
+    contents = soup.select(site_info[item])
+    if len(contents):
+      # Assume only the first result is relevant
+      # BS4 returns a list of results even if only 1 found
+      results[item] = contents[0].getText()
+  return results
+
 
 def get_texts(path, site_info):
   '''
@@ -76,7 +71,7 @@ def get_texts(path, site_info):
   Then extract text content, with metadata
   Return results
   '''
-  text = dict()
+  text = list()
   walkdir = os.path.join(os.path.abspath(path), site_info['url'])
   # Make sure paths don't have a trailing slash in the CSV
   paths_to_parse = site_info['paths'].split(',')
@@ -85,13 +80,21 @@ def get_texts(path, site_info):
     if root.replace(walkdir,'') in paths_to_parse or subdirs in paths_to_parse:
         files_to_parse = find_files(root, subdirs, files)
   for filename in files_to_parse:
-    if not site_info['name'] in text:
-      text[site_info['name']] = list()
-    text[site_info['name']].append(extract_text(site_info, filename))
+    text.append(extract_text(site_info, filename))
   return text
 
-def write_results(results):
-  pass
+def write_results(path, results):
+  if (not os.path.isdir(path)):
+    os.makedirs(path)
+  with open(os.path.join(path, 'metadata.csv'), 'w') as csvfile:
+    metadata = csv.writer(csvfile, quotechar='|', quoting=csv.QUOTE_ALL)
+    metadata.writerow(['site', 'title', 'date', 'author', 'filename'])
+    for site, rows in results.items():
+      for row in rows:
+        filename = os.path.join(path, row['title'])
+        metadata.writerow([site, row['title'], row['date'], row['author'], filename])
+        with open(filename, 'w') as content:
+          content.write(str(row['content']))
 
 def main():
   settings = get_settings()
@@ -99,7 +102,7 @@ def main():
   results = dict()
   for site in site_info:
     results[site['name']] = get_texts(settings.input_dir, site)
-  write_results(results)
+  write_results(settings.output_dir, results)
 
 if __name__ == '__main__':
   if sys.version_info[0] != 3:
