9688aede0bf4b4bb381e0cbcd78cb7e53721d0b0
Mike Widner <mikewidner@gmail.com>
2015-12-07 10:32:34 -0800

    Succesful sub-groupings by column

--- src/join_corpus.py
+++ src/join_corpus.py
@@ -2,7 +2,7 @@
 The Corpus Slicer
 
 Read in an CSV file of metadata describing corpus and file locations
-Organize and slice the text files for analysis and further processing
+Organize and combine the text files for analysis and further processing
 
 Mike Widner <mikewidner@stanford.edu>
 '''
@@ -12,6 +12,15 @@ import sys
 import argparse
 import pandas as pd
 from string import punctuation
+from collections import defaultdict
+
+def get_options():
+	parser = argparse.ArgumentParser(description='Slice corpus into files by site or author')
+	parser.add_argument('-i', dest='input_dir', required=True, help='Input directory where raw corpus exists')
+	parser.add_argument('-m', '--metadata', dest='metadata', required=True, help='A CSV file of metadata describing all the files')
+	parser.add_argument('-o', dest='output_dir', required=True, help='Output directory for results')
+	parser.add_argument('-c', '--column', dest='columns', required=True, action='append', help='The column(s) by which to slice up the corpus. Multiple columns will create sub-categories, e.g., -c col1 -col2 will slice corpus into all col2 within col1')
+	return parser.parse_args()
 
 def get_year(value):
 	'''
@@ -41,17 +50,21 @@ def clean_filename(filename):
 	remove_punct_map = dict.fromkeys(map(ord, punctuation.replace('_', '') + '’'))
 	return str(filename)[:100].translate(remove_punct_map).strip().replace(' ', '_')
 
-def generate_text(df, output_dir, dirname, filename):
+def get_uniques(df, key):
 	'''
-	Takes all words for a given slice
-	Write out as a single text file
+	Return an array of unique values for the given column name/key
+	'''
+	return(pd.unique(df[key].values.ravel()))
+
+def write_text(df, output_path, filename):
+	'''
+	Write out all words in a df as a single text file
 	'''
 	# clean up filenames
 	filename = clean_filename(filename)
 	if (len(df['words']) == 0 or len(filename) == 0):
 		return 	# don't write empty files
 
-	output_path = os.path.join(output_dir, dirname)
 	print("Generating {}/{}.txt".format(output_path, filename))
 	if not os.path.isdir(output_path):
 		os.makedirs(output_path)
@@ -60,19 +73,28 @@ def generate_text(df, output_dir, dirname, filename):
 		fh.write(row)
 	fh.close()
 
-def get_unique(df, key):
+def join_texts(df, columns, path, join_with = None):
 	'''
-	Return an array of unique values for the given column name/key
+	Generate a single text of all the words in the given column
+	Pass another df as join_with to perform a join
 	'''
-	return(pd.unique(df[key].values.ravel()))
+	for index, column in enumerate(columns):
+		path = os.path.join(path, column)
 
-def get_options():
-	parser = argparse.ArgumentParser(description='Slice corpus into files by site or author')
-	parser.add_argument('-i', dest='input_dir', required=True, help='Input directory where raw corpus exists')
-	parser.add_argument('-m', '--metadata', dest='metadata', required=True, help='A CSV file of metadata describing all the files')
-	parser.add_argument('-o', dest='output_dir', required=True, help='Output directory for results')
-	parser.add_argument('-c', '--column', dest='columns', required=True, action='append', help='The column(s) by which to slice up the corpus. Multiple columns will create sub-categories, e.g., -c col1 -col2 will slice corpus into all col2 within col1')
-	return parser.parse_args()
+		for item in get_uniques(df, column):
+			df_item = df[df[column] == item]
+
+			if join_with is not None and not df_item.empty:
+				df_left = df[df[join_with['column']] == join_with['item']]
+				df_item = pd.merge(df_left, df_item)
+
+			if index < len(columns) - 1:
+				join_texts(df, columns[index + 1:], os.path.join(path, item), {'column': column, 'item': item})
+
+			# if len(df_item.index):
+			write_text(df_item, path, item)
+
+
 
 def main():
 	'''
@@ -85,16 +107,11 @@ def main():
 	df['year'] = df['date'].apply(get_year)
 	df['basename'] = df['filename'].apply(os.path.basename)
 	df['words'] = df['filename'].apply(load_words)	# load words for every file
+	join_texts(df, options.columns, options.output_dir)
 
-	for column in options.columns:
-		# get a list of our unique values for each column
-		uniques = get_unique(df, column)
-		# now iterate through our desired columns and generate new text files
-		for item in uniques:
-			generate_text(df[df[column] == item], options.output_dir, column, item)
 
 if __name__ == '__main__':
 	if sys.version_info[0] != 3:
-	    print("This script requires Python 3")
-	    exit(-1)
+		print("This program requires Python 3.")
+		exit(-1)
 	main()
