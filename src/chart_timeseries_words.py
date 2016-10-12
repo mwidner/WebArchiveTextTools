8f46e94c1f92a86acbc4bef8e3a40e6a679856e3
Mike Widner <mikewidner@gmail.com>
2016-10-12 16:22:31 -0700

    Successfully generating time series charts

--- src/chart_timeseries_words.py
+++ src/chart_timeseries_words.py
@@ -8,8 +8,9 @@ import nltk
 import pandas as pd
 import argparse
 import dateutil
+import datetime
 import wordfreqs as wf
-import gen_charts as gc
+import matplotlib.pyplot as plt
 
 
 def get_settings():
@@ -19,21 +20,13 @@ def get_settings():
     parser.add_argument('-s', dest='show', action='store_true', help='Show images before saving')
     parser.add_argument('-n', dest='number', type=int, help='Top N items', default=50)
     parser.add_argument('-t', '--time', dest='time', type=int, help='Default intervals, in months', default=1)
+    parser.add_argument('-a', '--author', dest='author', help='Limit results to a specific author')
     parser.add_argument('-l', '--language', dest='language', required=True,
                         help='Language in which the texts were written',
                         choices=wf.lang_codes.keys(), type = str.lower)
-    parser.add_argument('-w', '--word', dest='word', action='append')
+    parser.add_argument('-w', '--word', dest='words', action='append')
+    parser.add_argument('--exact', dest='exact', action='store_true', default=False, help='Require an exact match')
     return parser.parse_args()
-
-
-def get_year(value):
-    datetime = dateutil.parser.parse(value)
-    return datetime.year
-
-
-def get_month(value):
-    datetime = dateutil.parser.parse(value)
-    return datetime.month
 
 
 def get_words(filename, language):
@@ -42,26 +35,50 @@ def get_words(filename, language):
     return wf.wordlist(sentences)
 
 
+def get_frequency(fd, word, exact):
+    freq = 0
+    if exact and word in fd:
+        freq = fd.freq(word)
+    else:
+        for token in fd:
+            if token.startswith(word):
+                freq += fd[token]
+    return freq
+
+
+def relative_frequency(row, words):
+    for word in words:
+        if len(row['words']):
+            row[word] = row[word] / len(row['words'])
+        else:
+            row[word] = 0
+    return row
+
 def main():
     settings = get_settings()
     if not os.path.isdir(settings.outputdir):
         os.makedirs(settings.outputdir)
 
     df = pd.read_csv(settings.inputfile, quotechar='|')
-    df['words'] = df['filename'].apply(get_words, args=(settings.language,))
-    df['year'] = df['date'].apply(get_year)
-    df['month'] = df['date'].apply(get_month)
-    df.dropna(subset=['words'], inplace=True)
-    words_df = df.groupby(by=['year', 'month'])['words']
-    print(words_df)
 
-    # for name, group in words_df:
-    #     documents = group.tolist()
-    #     tokens = list()
-    #     for words in documents:
-    #         tokens += words
-    #     group['fd'] = nltk.FreqDist(tokens)
-    #     print(group)
+    if settings.author:
+        df = df[df.author == settings.author]
+
+    df['date'] = df['date'].apply(lambda x: pd.to_datetime(str(x), format='%Y-%m-%d'))
+    df.set_index(['date'], inplace=True)
+    df.sort_index(inplace=True)
+    df['words'] = df['filename'].apply(get_words, args=(settings.language,))
+    df['fd'] = df['words'].apply(nltk.FreqDist)
+
+    for word in settings.words:
+        df[word] = df['fd'].apply(get_frequency, args=(word, settings.exact))
+    df = df.apply(relative_frequency, axis=1, args=(settings.words,))
+
+    tsres = df[settings.words].resample('M').median().interpolate('time')
+    lines = ['-.s', '--o', '-v', ':^']
+    tsres.plot(style=lines, color='k')
+    plt.savefig(os.path.join(settings.outputdir, '_'.join(settings.words)), pad_inches=.1, dpi=600)
+    # plt.show()
 
 
 if __name__ == '__main__':
